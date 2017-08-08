bf44b4c82e86739f982bb7be783b4e38f7e8cc44
Hugo Provencher <hugo1507@msn.com>
2017-08-08 19:18:10 -0400

    Add files via upload

---
+++ MidiCV.ino
@@ -0,0 +1,35 @@
+#include <MIDI.h>
+
+int OutVoltPin=11;
+
+int Voltage=0;
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void HandleNoteOn(byte channel, byte pitch, byte velocity){ 
+  double PITCH=(pitch-16)/(5588-16); //Based on https://en.wikipedia.org/wiki/Piano_key_frequencies The pitch should be between 16 and 5588
+  Voltage=PITCH*1023;
+  analogWrite(OutVoltPin,Voltage);
+}
+
+/* I don't know what this does
+void HandleCC(byte channel, byte pitch, byte velocity){
+  // Do something here with your data!
+} */
+
+void HandleNoteOff(byte channel, byte pitch, byte velocity){
+  analogWrite(OutVoltPin,0);
+}
+
+void setup() {
+  pinMode(OutVoltPin,OUTPUT);
+  
+  MIDI.setHandleNoteOn(HandleNoteOn); 
+  // MIDI.setHandleControlChange(HandleCC);
+  MIDI.setHandleNoteOff(HandleNoteOff);
+
+}
+
+void loop() {
+  MIDI.read();
+}
